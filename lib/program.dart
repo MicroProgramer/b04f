8d97d9d0b273b84013de82508de1b67e5799548b
MicroProgramer <microprogramer@yahoo.com>
2023-11-04 01:42:27 +0500

    updated

--- lib/program.dart
+++ lib/program.dart
@@ -1,24 +1,50 @@
 void main() async {
-  // var x = await sumNums(10, 20);
-  // print(x);
 
+  // String type = 'audio';    ///text, video, audio
+  //
+  //
+  // if (type == FileType.video.name) {
+  //   print("This is a video file");
+  // } else if (type == FileType.audio.name){
+  //   print('This is a music file');
+  // } else if (type == FileType.text.name){
+  //   print('This is a string file');
+  // } else {
+  //   print('This is an unknown type');
+  // }
 
-  print('before loop');
+  // switch (type) {
+  //   case FileType.video.name:
+  //     print("This is a video file");
+  //     break;
+  //   case FileType.audio.name:
+  //     print("This is a music file");
+  //     break;
+  //   case FileType.text.name:
+  //     print("This is a String file");
+  //     break;
+  //   default:
+  //     print("This is an unknown file");
+  // }
 
-  int i = 0;
-  Future.doWhile(() async {
-    print(i);
-    // await Future.delayed(Duration(milliseconds: 500));
-    i++;
-    return (i < 3);
-  });
+  var obj = TestClass("Ali", "98w38wybewuybe80woedu");
+  obj.logic = '2';
+}
 
-  print('after loop');
+class TestClass {
+  String name;
+  String _secret;// any secret value
+
+  TestClass(this.name, this._secret);
+
+  String get logic => _secret.toUpperCase();
+
+  void set logic (String x) {
+    _secret = x;
+  }
 
 }
 
-Future<int> sumNums(int a, int b) async {
-  await Future.delayed(Duration(seconds: 1));
-  await Future.delayed(Duration(seconds: 1));
-  return a + b;
+enum FileType {
+  text, video, audio
 }
